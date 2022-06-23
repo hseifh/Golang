65c64305d092820b0d889413f8e25719eff0189d
tapple <hseifhegruh@gmail.com>
2022-06-23 23:04:34 +0900

    go

---
+++ greeting.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World❕❕")
+
+	fmt.Println("Good morning")
+
+	fmt.Println("Good afternoon")
+
+	fmt.Println("Good evening")
+}
